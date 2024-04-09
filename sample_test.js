77487535ed6801202bb317d4f5be87d12ff9b612
Ariana <136840493+aoverko@users.noreply.github.com>
2024-04-09 11:44:21 -0400

    Update sample_test.js

--- sample_test.js
+++ sample_test.js
@@ -2,22 +2,21 @@
 //double check if comment inits = strings
 
 const tokens = [
-    //TECHNICALLY don't tokenize these but just in case:
-
-    // {"type" : type.STRING, "value" : "//"},
-    // {"type" : type.STRING, "value" : "This"},
-    // {"type" : type.STRING, "value" : "is"},
-    // {"type" : type.STRING, "value" : "a"},
-    // {"type" : type.STRING, "value" : "single"},
-    // {"type" : type.STRING, "value" : "comment"},
-    // {"type" : type.STRING, "value" : "/*"},
-    // {"type" : type.STRING, "value" : "This"},
-    // {"type" : type.STRING, "value" : "is"},
-    // {"type" : type.STRING, "value" : "a"},
-    // {"type" : type.STRING, "value" : "multi"},
-    // {"type" : type.STRING, "value" : "line"},
-    // {"type" : type.STRING, "value" : "comment"},
-    // {"type" : type.STRING, "value" : "*/"}, 
+    //COMMENTS
+    {"type" : type.STRING, "value" : "//"},
+    {"type" : type.STRING, "value" : "This"},
+    {"type" : type.STRING, "value" : "is"},
+    {"type" : type.STRING, "value" : "a"},
+    {"type" : type.STRING, "value" : "single"},
+    {"type" : type.STRING, "value" : "comment"},
+    {"type" : type.STRING, "value" : "/*"},
+    {"type" : type.STRING, "value" : "This"},
+    {"type" : type.STRING, "value" : "is"},
+    {"type" : type.STRING, "value" : "a"},
+    {"type" : type.STRING, "value" : "multi"},
+    {"type" : type.STRING, "value" : "line"},
+    {"type" : type.STRING, "value" : "comment"},
+    {"type" : type.STRING, "value" : "*/"}, 
 
     //TERMITE.LOG
     //Separate indiv methods based on ( )... not sure if in "" or not
