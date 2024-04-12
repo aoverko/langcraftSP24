e9334686f5d5f1cc0f8d6988e9191dbda4c25889
Ariana <136840493+aoverko@users.noreply.github.com>
2024-04-12 09:36:49 -0400

    Finished testcase draft

--- sample_test.js
+++ sample_test.js
@@ -21,11 +21,49 @@ const tokens = [
     //TERMITE.LOG
     //Separate indiv methods based on ( )... not sure if in "" or not
     {"type" : type.METHOD, "value" : termite.log},
-    {"type" : type.DELIMITER, "value" : "("},
+    {"type" : type.DELIMITER, "value" : (},
     {"type" : type.STRING, "value" : "Hello,"},
     {"type" : type.STRING, "value" : "world!"},
-    {"type" : type.DELIMITER, "value" : ")"},
-    {"type" : type.TERMINATOR, "value" : "|"}
+    {"type" : type.DELIMITER, "value" : )},
+    {"type" : type.TERMINATOR, "value" : |},
 
     //#PROJECT
+    {"type" : type.VARIABLE, "value" : set},
+    {"type" : type.IDENTIFIER, "value" : #project},
+    {"type" : type.ASSIGNMENT, "value" : =},
+    {"type" : type.STRING, "value" : "CodeNetLang"},
+    {"type" : type.TERMINATOR, "value" : |},
+
+    //#ADD -- figure out how to separate the elements w|out spaces here
+    {"type" : type.FUNCTION, "value" : def},
+    {"type" : type.IDENTIFIER, "value" : #add},
+    {"type" : type.DELIMITER, "value" : (},
+    {"type" : type.PARAM, "value" : a},
+    {"type" : type.DELIMITER, "value" : ,},
+    {"type" : type.PARAM, "value" : b},
+    {"type" : type.DELIMITER, "value" : )},
+    {"type" : type.DELIMITER, "value" : :|},
+    {"type" : type.KEYWORD, "value" : Return},
+    {"type" : type.PARAM, "value" : a},
+    {"type" : type.OPERATOR, "value" : +},
+    {"type" : type.PARAM, "value" : b},
+    {"type" : type.TERMINATOR, "value" : |},
+    {"type" : type.DELIMITER, "value" : |:},
+
+    // #RESULT
+    {"type" : type.VARIABLE, "value" : set},
+    {"type" : type.IDENTIFIER, "value" : #result},
+    {"type" : type.ASSIGNMENT, "value" : =},
+    {"type" : type.IDENTIFIER, "value" : #add},
+    {"type" : type.DELIMITER, "value" : (},
+    {"type" : type.PARAM, "value" : 5},
+    {"type" : type.DELIMITER, "value" : ,},
+    {"type" : type.PARAM, "value" : 3},
+    {"type" : type.DELIMITER, "value" : )},
+    {"type" : type.TERMINATOR, "value" : |},
+    {"type" : type.METHOD, "value" : termite.log},
+    {"type" : type.DELIMITER, "value" : (},
+    {"type" : type.IDENTIFIER, "value" : #result},
+    {"type" : type.DELIMITER, "value" : )},
+    {"type" : type.TERMINATOR, "value" : |}
 ]
